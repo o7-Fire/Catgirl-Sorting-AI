55b4178f662ad5f19efe704bf2fd2069b5bbc2d4
Depressee-Chan <60305140+Depressee-Chan@users.noreply.github.com>
2020-10-17 18:27:51 -0600

    Update and rename TF_IMG_Classifier.py to main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 import shutil
 
 #Zip Data Path
-training_data_file = "./IMG Sorter/train.zip"
+training_data_file = "C:/Path/To/IMG Sorter/train.zip"
 
 with zipfile.ZipFile(training_data_file, 'r') as z:
     z.extractall()
@@ -27,12 +27,13 @@ def preprocess_image(img, side=96):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img/255.0
 
-train_path = "./train/" #Zip File Dir
+train_path = "C:/Path/To/IMG Sorter/train/" #Zip File Dir
 image_files = os.listdir(train_path)
 train_images = [load_data(train_path + file) for file in image_files]
 train_labels = [extract_label(file) for file in image_files]
 
-preview_index = 21000
+
+preview_index = 200
 plt.subplot(1,2,1)
 plt.imshow(train_images[preview_index])
 plt.subplot(1,2,2)
@@ -65,39 +66,37 @@ model = tf.keras.Sequential(layers)
 model.compile(optimizer = tf.optimizers.Adam(),
               loss = tf.losses.SparseCategoricalCrossentropy(),
               metrics = [tf.metrics.SparseCategoricalAccuracy()])
-model.fit(train_images, train_labels, epochs=5, batch_size=50)#Epochs Are How Many Times Training Occurs
-model.save_weights("model.tf")#Saving Trained Model
+model.fit(train_images, train_labels, epochs=1, batch_size=50)#Epochs Are How Many Times Training Occurs
+model.save_weights("C:/Path/To/IMG Sorter/model.h5")#Saving trained model to current directory
 
-#Evaluate CNN On New Pictures
-nsfw_path = "./NSFW/"
-sfw_path = "./SFW/"
-eval_path = "./New_Data/"
+#Setting paths to image folders
+nsfw_path = "C:/Path/To/IMG Sorter/Sorted_NSFW/"
+sfw_path = "C:/Path/To/IMG Sorter/Sorted_SFW/"
+eval_path = "C:/Path/To/IMG Sorter/Unsorted_Data/"
+all_path = "C:/Path/To/IMG Sorter/All_Data/"
 
 list1 = os.listdir(all_path)
-num_files = (len(list1))
-num_files = num_files
+num_files = (len(list1))#Get num of files in all_data path
+num_files = num_files#Not sure why I put this here but it might be important so I'll leave it
 
 eval_files = os.listdir(eval_path)
 eval_images = [preprocess_image(load_data(eval_path + file)) for file in eval_files]
 eval_model = tf.keras.Sequential(layers)
-eval_model.load_weights("model.tf")
+eval_model.load_weights("C:/Path/To/IMG Sorter/model.h5")#Load pretrained model
 eval_predictions = eval_model.predict(np.expand_dims(eval_images, axis=-1))
 
-###Start AI Evaluation Cycle###
-#Classifying New Data
+###Start CNN Evaluation Cycle###
 for i in range(len(eval_images)):
-    files = i + num_files
-    #Data Labels
-    title = ("nsfw" if np.argmax(eval_predictions[i])==1 else "sfw")
-    #Sending Classified Data To Respective Folders
+    files = i
     file_address = (str(files) + ".jpg")
     file_path = (eval_path + file_address)
-    if np.argmax(eval_predictions[i])==1:
-        shutil.move(file_path, nsfw_path)
 
+    #Sending Sorted Data To Respective Folders
+    if np.argmax(eval_predictions[i])==1:#Passing unsorted data to model
+        shutil.move(file_path, nsfw_path)#Move file to nsfw folder
 
     else:
-        shutil.move(file_path, sfw_path)
+        shutil.move(file_path, sfw_path)#move data to sfw folder
 
 print("---ALL TASKS COMPLETE!---")
-###End AI Evaluation Cycle###
+###End CNN Evaluation Cycle###
